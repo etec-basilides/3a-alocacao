b3c15e2f779a48d25480c0f43af4b5dbd163d09f
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2023-03-14 14:05:35 -0300

    inclusao serviço lista-carros

---
+++ alocacao/src/main/java/br/gov/sp/etec/alocacao/model/Cliente.java
@@ -0,0 +1,43 @@
+package br.gov.sp.etec.alocacao.model;
+
+public class Cliente {
+	
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String tel;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+		
+
+}
